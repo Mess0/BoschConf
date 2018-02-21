ed4a4c893dbca5084e90bc0a166e741c2ef4c7dd
Tasso Mulzer <tasso.mulzer@beuth-hochschule.de>
2018-02-21 22:12:37 +0100

    Get distance and / or switch state from kafka.

--- python2_tmu.py
+++ python2_tmu.py
@@ -2,14 +2,29 @@ import sys, json
 from kafka import KafkaConsumer
 
 
-# fp = open("example.json", "r")
+# fp = open("C:/Users/Tasso/Documents/BCW18/bla.json", "r")
 
 # join a consumer group for dynamic partition assignment and offset commits
 
-consumer = KafkaConsumer('dredd', group_id = 'Clusters', bootstrap_servers=['100.102.4.11:9092'], auto_offset_reset='earliest')
+consumer = KafkaConsumer('dreddswitch', group_id = 'Clusters', bootstrap_servers=['100.102.4.11:9092'], auto_offset_reset='earliest')
+
+distance = 0
+switch = 0
 
 
 for msg in consumer:
     data = json.loads(msg.value)
-    for distance in data["measurements"][0]["series"]["Balluff Master: Distance on Port 0"]:
-        print(json.dumps(distance))
+    for meas in data["measurements"]:
+        try:
+            val = meas["series"]["Balluff Master: Distance on Port 0"]
+            distance = val
+            print(json.dumps(distance))
+        except:
+            pass
+
+        try:
+            val = meas["series"]["Balluff Master: Switch state on Port 0"]
+            switch = val
+            print(json.dumps(switch))
+        except:
+            pass
