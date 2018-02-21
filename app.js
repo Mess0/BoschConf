49ce0691df44630f70d6692044f74b6562284967
Matthias Eckhart <5939067+MatthiasEckhart@users.noreply.github.com>
2018-02-21 13:57:45 +0100

    Removed unnecessary code.

--- app.js
+++ app.js
@@ -3,11 +3,6 @@
 var kafka = require('kafka-node'),
     client = new kafka.Client('100.102.4.11:2181'),
     Consumer = kafka.Consumer,
-    //Producer = kafka.Producer,
-    //  producer = new Producer(client),
-    //  payloads = [
-    //      {topic: 'DREDD', messages: 'DREDD', partition: 0}
-    //  ],
     consumer = new Consumer(
         client,
         [
@@ -17,18 +12,6 @@ var kafka = require('kafka-node'),
             autoCommit: false
         }
     );
-
-//producer.on('ready', function () {
-//    producer.send(payloads, function (err, data) {
-//        console.log(data);
-//    });
-//});
-
-
-//producer.on('error', function (err) {
-//    console.log(err);
-//});
-//
 
 consumer.on('message', function (message) {
     console.log(message.value);
