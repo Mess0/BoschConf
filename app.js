8441ded027f17fa65befc23d5111ba83d402afbd
Matthias Eckhart <5939067+MatthiasEckhart@users.noreply.github.com>
2018-02-21 14:21:10 +0100

    Integrated python call.

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 'use strict';
 
+const host = '100.102.4.11:2181';
+
+var Promise = require('promise');
+
+function getRunPyPromise(pyProgramPath) {
+    return new Promise(function (success, failed) {
+
+        var ch_ps = require('child_process');
+        var pyprog = ch_ps.spawn('python', [pyProgramPath]);
+
+        pyprog.stdout.on('data', function (data) {
+            success(data);
+        });
+
+        pyprog.stderr.on('data', function (data) {
+            failed(data);
+        });
+
+    });
+}
+
 var kafka = require('kafka-node'),
-    client = new kafka.Client('100.102.4.11:2181'),
+    client = new kafka.Client(host),
     Consumer = kafka.Consumer,
     consumer = new Consumer(
         client,
         [
-            {topic: 'AI_Factory', partition: 0}
+            {topic: 'dredd', partition: 0}
         ],
         {
             autoCommit: false
@@ -14,9 +35,17 @@ var kafka = require('kafka-node'),
     );
 
 consumer.on('message', function (message) {
+
+    getRunPyPromise('./python1.py').then(function (fromPy) {
+        console.log(fromPy.toString());
+    }, function (err) {
+        console.log(err);
+    });
+
     console.log(message.value);
 });
 
 consumer.on('error', function (err) {
     console.log(err);
 });
+
