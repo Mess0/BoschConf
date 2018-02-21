6945ca499b2eda74c8a7d5373163df75e1566798
Tasso Mulzer <tasso.mulzer@beuth-hochschule.de>
2018-02-21 16:16:10 +0100

    Get values directly from kafka.

--- python2_tmu.py
+++ python2_tmu.py
@@ -1,7 +1,15 @@
 import sys, json
+from kafka import KafkaConsumer
 
 
-fp = open("example.json", "r")
-data = json.load(fp)
-for distance in data["measurements"][0]["series"]["Dredd Balluff master: Distance on Port 0"]:
-    print(json.dumps(distance))
+# fp = open("example.json", "r")
+
+# join a consumer group for dynamic partition assignment and offset commits
+
+consumer = KafkaConsumer('dredd', group_id = 'Clusters', bootstrap_servers=['100.102.4.11:9092'], auto_offset_reset='earliest')
+
+
+for msg in consumer:
+    data = json.loads(msg.value)
+    for distance in data["measurements"][0]["series"]["Balluff Master: Distance on Port 0"]:
+        print(json.dumps(distance))
